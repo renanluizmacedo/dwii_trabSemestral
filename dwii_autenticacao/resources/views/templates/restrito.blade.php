f25dd78bd762f3b325f5fe358a8a3d28cb55cd11
robertera <robertokorzun@hotmail.com>
2022-08-31 08:23:48 -0300

    Commit Final

--- dwii_autenticacao/resources/views/templates/restrito.blade.php
+++ dwii_autenticacao/resources/views/templates/restrito.blade.php
@@ -2,15 +2,17 @@
 
 @section('conteudo')
 
-<h1>Você é fraco lhe falta acesso</h1>
+<h1 style="text-align:center; font-size:30px; color:red">Calma ai amigão!, aparentemente você não tem permissão</h1>
 <hr>
-<a class="btn btn-dark btn-block align-content-center">
-    <img src="https://criticalhits.com.br/wp-content/uploads/2019/01/Sasuke_jovem_vs_Itachi.png" alt="height= 1000" width="1000">
+<a href="{{route('index')}}" style="margin-left:auto; margin-right:auto;" class="btn btn-dark btn-block align-content-center">
+    <img src="https://cdna.artstation.com/p/assets/images/images/024/250/174/large/ijaz-tist-chungus-2.jpg?1581801507" alt="height= 350" width="350">
     <br>
     <br>
-    
-   
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-left-square-fill" viewBox="0 0 16 16">
+        <path d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1z" />
+    </svg>
+    &nbsp; Retornar
 
 </a>
 
-@endsection
+@endsection 
